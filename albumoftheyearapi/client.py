21a7333a5100101f2bb2600ef2966ece7ac86658
Juan Sebastian Peña <jspenaq@unal.edu.co>
2025-05-29 18:28:56 -0500

    docs: Add docstrings to AOTY class and init method

--- albumoftheyearapi/client.py
+++ albumoftheyearapi/client.py
@@ -6,17 +6,26 @@ from albumoftheyearapi.album import AlbumMethods
 
 
 class AOTY(UserMethods, ArtistMethods, AlbumMethods):
-    """A light weight python library that acts as an API for https://www.albumoftheyear.org"""
+    """
+    A lightweight Python library that acts as an API for https://www.albumoftheyear.org.
 
-    def __init__(self):
-        """Initializes the required variables for getting website data.
-        Required for easier caching
+    This class inherits methods from UserMethods, ArtistMethods, and AlbumMethods
+    to provide a comprehensive interface for interacting with the Album of the Year website.
+    """
+
+    def __init__(self) -> None:
         """
-        self.user = ""
-        self.artist = ""
-        self.url = ""
-        self.user_url = "https://www.albumoftheyear.org/user/"
-        self.artist_url = "https://www.albumoftheyear.org/artist/"
-        self.upcoming_album_class = "albumBlock five small"
-        self.aoty_albums_per_page = 60
-        self.page_limit = 21
+        Initializes the AOTY client with default attributes.
+
+        These attributes store base URLs and other configuration details
+        required for scraping and data retrieval from the Album of the Year website.
+        They are also set up to facilitate easier caching of page content.
+        """
+        self.user: str = ""
+        self.artist: str = ""
+        self.url: str = ""
+        self.user_url: str = "https://www.albumoftheyear.org/user/"
+        self.artist_url: str = "https://www.albumoftheyear.org/artist/"
+        self.upcoming_album_class: str = "albumBlock five small"
+        self.aoty_albums_per_page: int = 60
+        self.page_limit: int = 21
